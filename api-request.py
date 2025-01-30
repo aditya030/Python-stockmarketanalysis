64a36d1697547021acdca92dd3e63674e0294741
ADITYA SINHA <adityasinha@ADITYAs-MacBook-Pro.local>
2025-01-30 23:24:35 +0530

    new file

--- api-request.py
+++ api-request.py
@@ -4,7 +4,7 @@ from jugaad_data.nse import bhavcopy_save, bhavcopy_fo_save, stock_df
 # Download bhavcopy
 # bhavcopy_save(date(2023,1,7), "./")
 
-df = stock_df(symbol="COALINDIA", from_date=date(2019,1,1),
+df = stock_df(symbol="SBIN", from_date=date(2019,1,1),
             to_date=date(2020,1,30), series="EQ")
 
 print(df)
